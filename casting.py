6c1de144c8acea287fe58889cc04d7559b47c27b
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-07 09:39:40 -0300

    Update casting.py

--- casting.py
+++ casting.py
@@ -1,3 +1,11 @@
 def casting():
     """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
-    pass
+    gasto= float(input("ingrese un gasto: "))
+    recibido= float(input("ingrese el dinero recibido: "))
+    vuelto= recibido-gasto
+    pesos = int(vuelto)
+    centavos = round((vuelto - pesos) * 100)
+    
+    print("Vuelto total:", vuelto)
+    print("Pesos:", pesos)
+    print("Centavos:", centavos)
