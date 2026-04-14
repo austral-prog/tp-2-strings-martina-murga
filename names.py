91227b2a21a5926feeebb3b6547804000b91f22c
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-14 20:00:44 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,11 +2,10 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    nombre=input("Nombre: ")
-    apellido=input("Apellido: ")
-    nombre_completo = f"{nombre} {apellido}"
+    nombre=input()
+    apellido=input()
+    nombre_completo=(f"{nombre} {apellido}")
     print(f"{nombre_completo.lower()}")
     print(f"{nombre_completo.title()}")
     print(f"{nombre_completo.upper()}")
     print(f"\t{nombre}{apellido}")
-
